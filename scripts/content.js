5f485568501d28a9e489745649e7b50ce1647837
Joris <github@boekelcity.nl>
2023-05-15 17:06:06 +0200

    Add button element

---
+++ scripts/content.js
@@ -0,0 +1,34 @@
+const observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (!mutation.addedNodes) {
+      return;
+    }
+    for (let i = 0; i < mutation.addedNodes.length; i++) {
+      const node = mutation.addedNodes[i];
+      if(node.classList) {
+        if (node.classList.contains("ytd-grid-video-renderer") && node.tagName === 'YTD-MENU-RENDERER') {
+          console.log('joris'+i,node);
+          let hideButton = document.createElement('div');
+          hideButton.classList.add('youtube-simplehide-button')
+
+          let xmlString = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M12 19c.946 0 1.81-.103 2.598-.281l-1.757-1.757c-.273.021-.55.038-.841.038-5.351 0-7.424-3.846-7.926-5a8.642 8.642 0 0 1 1.508-2.297L4.184 8.305c-1.538 1.667-2.121 3.346-2.132 3.379a.994.994 0 0 0 0 .633C2.073 12.383 4.367 19 12 19zm0-14c-1.837 0-3.346.396-4.604.981L3.707 2.293 2.293 3.707l18 18 1.414-1.414-3.319-3.319c2.614-1.951 3.547-4.615 3.561-4.657a.994.994 0 0 0 0-.633C21.927 11.617 19.633 5 12 5zm4.972 10.558-2.28-2.28c.19-.39.308-.819.308-1.278 0-1.641-1.359-3-3-3-.459 0-.888.118-1.277.309L8.915 7.501A9.26 9.26 0 0 1 12 7c5.351 0 7.424 3.846 7.926 5-.302.692-1.166 2.342-2.954 3.558z"></path></svg>';
+          let svg = new DOMParser().parseFromString(xmlString, "text/xml");
+
+          hideButton.appendChild(svg.documentElement)
+          node.appendChild(hideButton);
+
+          node.style['height'] = '100%';
+        }
+        // if (mutation.addedNodes[i].classList.contains("ytd-video-renderer")) {
+        //   // Your logic here
+        //   console.log('joris hoi shelf '+i,mutation.addedNodes[i])
+        // }
+      }
+    }
+  });
+});
+
+observer.observe(document, {
+  childList: true,
+  subtree: true
+});
